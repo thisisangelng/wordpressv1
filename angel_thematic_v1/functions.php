6e3cfda0a79f38f23b3d8068b69af5365380c551
angelng <angel.ng.27@gmail.com>
2010-04-13 09:29:01 -0400

    04.13 Update

--- angel_thematic_v1/functions.php
+++ angel_thematic_v1/functions.php
@@ -21,48 +21,41 @@
 //}
 //add_filter('wp_page_menu_args','childtheme_menu_args');
 
-/* This is the datestamp feature - it is currently not working properly. I would
-like it to only show up on the homepage, but it shows up on every page
-ncluding the wp dashboard.*/
-
-?>
-    <div class="datestamp">
+function childtheme_datestamp() {
+    if (is_home() & !is_paged()) { ?>
+        <div class="datestamp">
 <?php
-        //if( is_front_page() ) :
-        
-        // This function inputs the date today. Set the default timezone to use.
-        date_default_timezone_set('America/New_York');
-        // To edit the style of datestamp refer to http://php.net/manual/en/function.date.php
-        echo date('l F j, Y, g:i a');
+// This function inputs the date today. Set the default timezone to use.
+    date_default_timezone_set('America/New_York');
+// To edit the style of datestamp refer to http://php.net/manual/en/function.date.php
+    echo date('l F j, Y, g:i a');
 ?>
     </div>
-    
-<?php //endif;
-?>
-
-<?php
+<?php }
+    }
+add_action('thematic_aboveheader','childtheme_datestamp');
 
 // Creating column headers for my categories. First define the function:
     function category_titles() {
 ?>
 <!-- Use html to create category titles -->
     <div id="categoryheader">
-
+        
         <div id="creativecategory" class="floatleft span-14 append-1 categorytitle">
            <p><a href="http://localhost:8888/blog/category/creative-inspiration/">CREATIVE <br>INSPIRATIONS</p>
         </div>
-    
+        
         <div id="cravingscategory" class="floatright span-14 append-1 categorytitle">
             <p><a href="http://localhost:8888/blog/category/cravings/">FOOD <br>CRAVINGS</p>
         </div>
-    
+        
         <div id="randomcategory" class="floatright span-14 last categorytitle">
             <p><a href="http://localhost:8888/blog/category/random/">RANDOM <br>THINGS I LIKE</p>
         </div>
-
+        
     </div>
 <?php }
- 
+
 // Now add the new function to the Thematic Action Hook.
     add_action('thematic_belowheader','category_titles');
 
@@ -71,13 +64,11 @@ ncluding the wp dashboard.*/
 ?>
 <!-- Use html to create the divs and assign classes, then php to create the loop. -->
     <div id="blogcontent">
-    <div id="creativecategory" class="category floatleft span-14 append-1">
-        
-        <!-- This excludes posts from categories id=5,6 from appearing in this div. -->
-        <?php query_posts($query_string . '&cat=-5,-6');
+    <div id="creativecategory" class="category floatleft span-14 append-1">       
+        <!-- This shows posts only from category id=4 -->
+        <?php query_posts($query_string . '&cat=4');
         // This is the start of the loop.
         if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        
             <div class="post">
                 <!-- Display the Title as a link to the Post's permalink. -->
                 <h2 class="entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
@@ -88,25 +79,22 @@ ncluding the wp dashboard.*/
                     </div>
                 <p class="entry-meta">Posted in <?php the_category(', '); ?></p>
             </div> <!-- closes the first div box -->
-       
        <!--This is how to end a loop -->
         <?php endwhile; else: ?>
             <p>Sorry, no posts matched your criteria.</p>
         <?php endif; ?>
     </div>
-
 <?php }
-    //Add the function to the Action Hook.
+
+//Add the function to the Action Hook.
     add_action('thematic_abovecontainer','creative_category');
     
 //Creating a div to hold all food category posts.
     function food_category () {
 ?>
-    <div id="foodcategory" class="category span-14 append-1 floatright">
-        
-        <?php query_posts($query_string . '&cat=-4,-6');
+    <div id="foodcategory" class="category span-14 append-1 floatright"> 
+        <?php query_posts($query_string . '&cat=5');
         if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-       
             <div class="post">
                 <!-- Display the Title as a link to the Post's permalink. -->
                 <h2 class="entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
@@ -117,32 +105,24 @@ ncluding the wp dashboard.*/
                     </div>
                 <p class="entry-meta">Posted in <?php the_category(', '); ?></p>
             </div> <!-- closes the first div box -->
-            
         <?php endwhile; else: ?>
             <p>Sorry, no posts matched your criteria.</p>
         <?php endif; ?>
     </div>
-
+    </div>
 <?php }
-
     add_action('thematic_abovecontainer','food_category');
 
-?>
-    </div>
-    
-<?php
-
-//* To create a new loop we have to get rid of thematic index loop first.*//
+// To create a new loop we have to get rid of thematic index loop first.
     function remove_index_loop() {
         remove_action('thematic_indexloop', 'thematic_index_loop');
 }
-    add_action('init', 'remove_index_loop');
- 
+    add_action('init', 'remove_index_loop'); ?>
+<?php
 // Now we will create the new loop.
     function random_category_loop(){
-        query_posts($query_string . '&cat=-5,-4'); ?>
+        query_posts($query_string . '&cat=6'); ?>
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
- 
             <div class="post">
                 <!-- Display the Title as a link to the Post's permalink. -->
                 <h2 class="entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
@@ -153,12 +133,10 @@ ncluding the wp dashboard.*/
                     </div> 
                 <p class="entry-meta">Posted in <?php the_category(', '); ?></p>
             </div> <!-- closes the first div box -->
-       
         <?php endwhile; else: ?>
             <p>Sorry, no posts matched your criteria.</p>
         <?php endif; ?>
     </div>
-
 <?php }
 // And in the end activate the new loop.
 add_action('thematic_indexloop', 'random_category_loop');
